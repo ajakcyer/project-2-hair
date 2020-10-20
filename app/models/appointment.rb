6d7e356526ca9f5cbab3ef7f225f9b5a10dfba35
ajakcyer <ajakcyer97@gmail.com>
2020-10-20 11:09:14 -0400

    appointment stylist name drop down

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,5 +3,12 @@ class Appointment < ApplicationRecord
     belongs_to :stylist
     has_many :reviews
 
+    def stylist_name=(name)
+        self.stylist = Stylist.find_by(name: name)
+    end
+
+    def stylist_name
+        self.stylist.name
+    end
     
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,5 @@
 class Review < ApplicationRecord
     belongs_to :appointment
     belongs_to :user
-
     
 end
