240e8afc2353d93bb266004190ca51a062c39e61
SDBoahen <SDBoahen@gmail.com>
2020-10-19 21:25:42 -0400

    Seeded some Users, Stylists, Styles, and StylistStyles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+
+
+# ajak = User.create(name: "Ajak")
+# sam =  User.create(name: "Sam")
+
+
+
+
+# 5.times do
+
+#     Stylist.create(name: Faker::FunnyName.unique.two_word_name, years_of_experience: rand(1..10))
+
+# end
+
+
+# 50.times do
+
+#     Style.create(name: Faker::FunnyName.unique.name, description: Faker::JapaneseMedia::Naruto.unique.character)
+
+# end
+
+
+100.times do 
+
+    StylistStyle.create(stylist_id: Stylist.all.sample.id, style_id: Style.all.sample.id, price: rand(1..100), style_img_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRX_Ncyqa8M7lBrXwuJzYZ9rPMKGarDzEOUGQ&usqp=CAU")
+
+end 
