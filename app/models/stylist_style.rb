0e7e206bcb8e437f181371e6b08211360bcbc803
ajakcyer <ajakcyer97@gmail.com>
2020-10-22 01:24:41 -0400

    added validation so price cant be more than 1000 or less than 1 to appointment model

--- app/models/stylist_style.rb
+++ app/models/stylist_style.rb
@@ -4,6 +4,9 @@ class StylistStyle < ApplicationRecord
 
     ##MAYBE SHOULD WORK SO EVERY STYLIST CAN ONLY HAVE ONE SPECIFIC STYLE
     validates :style, uniqueness: {scope: :stylist}
+    validates :price, numericality: {greater_than: 1, less_than: 1000}
+
+    
 
     def style_name=(name)
         self.style = Style.find_or_create_by(name: name)
