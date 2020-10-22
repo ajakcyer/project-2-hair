73feed1600ce9bd292bdcc0dea0fc9ec1cd45a6a
SDBoahen <SDBoahen@gmail.com>
2020-10-22 18:40:28 -0400

    Added Validations to Stylist Signup and Login for name and username, Designed User Show Page, Fixed bug with Stylist Show Page regarding proper iteration for Stylist rating based on Appointment Reviews

--- app/models/stylist.rb
+++ app/models/stylist.rb
@@ -6,20 +6,27 @@ class Stylist < ApplicationRecord
     has_many :styles, through: :stylist_styles
 
     has_secure_password
+    validates :name, :username, presence: true
 
     
     def stylist_rating
+
         review_array = []
          self.appointments.select do |app|
             if app.reviews[0]
                 review_array << app.reviews[0]
             end
          end
+
         total_appointments_with_reviews = review_array.count
+
         if review_array.present?
             
-            sum_of_ratings     = self.appointments.collect{ |app| app.reviews[0].rating }.sum
-    
+            sum_of_ratings = review_array.sum do |review| 
+                
+                review.rating
+            end
+
             sum_of_ratings/total_appointments_with_reviews
     
         else 
